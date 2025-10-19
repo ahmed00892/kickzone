a118e2ab256b2d548dc89e52eb9af099a2e744ef
Mohamed Adham <madham2004@gmail.com>
2025-10-19 14:31:39 +0300

    Initial project commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/LogIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,8 @@
+const LogIn = () => {
+  return (
+    <div>
+      <h1>LogIn</h1>
+    </div>
+  );
+};
+export default LogIn;
