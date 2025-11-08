973a248f86d23a4cc726428b9ea913a43a9ea739
ahmed00892 <ahmedhassan.1142006@gmail.com>
2025-11-08 02:16:38 +0200

    Clean up duplicated imports in App.jsx
    Removed unused imports for EditStadium and Route.

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ import Profile from "./pages/Profile.jsx";
 import EditProfile from "./pages/EditProfile.jsx";
 import Contact from "./pages/Contact.jsx";
 import About from "./pages/About.jsx";
-import Cart from "./pages/Cart.jsx";
-import EditStadium from "./pages/EditStadium.jsx";
-import { Route, Routes } from "react-router-dom";
-<pages />;
-
 import Stadium from "./pages/stadiums/Stadium";
 import StadiumDetails from "./pages/stadiums/StadiumDetails";
 import Cart from "./pages/Cart.jsx";
